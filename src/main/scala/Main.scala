7eb9d9b2e40911323b044221c197dea69f463a96
s368 <s368s368s@gmail.com>
2019-02-26 09:16:00 +0100

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import example.Lists
+
+object Main extends App {
+    println("Hey!")
+    
+    println(Lists.sum(List(1,2,3)))
+    println(Lists.max(List(1,2,3)))
+    
+    println(Lists.sum(List(1,2,3)))
+    println(Lists.max(List(1,2,3)))
+
+    println(Lists.sum(List()))
+    println(Lists.max(List()))
+}
